ea9f12b01e1c28a581e8442d536be9ef67aba42f
Gevorg Adamyan <adamyangevorg4@gmail.com>
2023-02-23 15:24:41 +0400

    add project files

--- Support/Data.cs
+++ Support/Data.cs
@@ -5,7 +5,6 @@ namespace SeleniumVScode.Support
         public static string username = "John Doe";
         public static string password = "ThisIsNotAPassword";
         public static string[] programs = {"Medicare", "Medicaid", "None"};
-
         public static string[] facilityNames = {"Tokyo CURA Healthcare Center", "Hongkong CURA Healthcare Center", "Seoul CURA Healthcare Center"};
 
     }
